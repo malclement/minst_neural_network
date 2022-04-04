419188a294e345f2bdd7224f6bc79e7ada1372d7
malclement <92513462+malclement@users.noreply.github.com>
2022-04-04 14:15:55 +0200

    Minor corrections

--- MinstNeuralNetwork/image_processing.py
+++ MinstNeuralNetwork/image_processing.py
@@ -9,10 +9,7 @@ image_size = 28
 def image_process(path):
     img = load_sample_image(path)
     img = reformat_image(img)
-    img = inverse_image(img)
-    img = zero_to_ones(img)
-    x_train = matrix_to_vector(img)
-    return x_train
+    return img
 
 
 def load_sample_image(path):
@@ -22,8 +19,9 @@ def load_sample_image(path):
 
 
 def inverse_image(img):
-    for i, j in range(image_size, image_size):
-        img[i, j] = 255 - img[i, j]
+    for i in range(len(img[0])):
+        for j in range(len(img[0])):
+            img[i][j] = 255 - img[i][j]
     return img
 
 
@@ -31,7 +29,7 @@ def inverse_image(img):
 def reformat_image(image):
     image = cv2.resize(image, (image_size, image_size), interpolation=cv2.INTER_LINEAR)
     image = cv2.bitwise_not(image)
-    image = numpy.divide(image, 255)
+    # image = numpy.divide(image, 255)
     return image
 
 
@@ -50,3 +48,5 @@ def convert_to_grey_levels(image):
 def zero_to_ones(image):
     image[image == 0] = 1
     return image
+
+
